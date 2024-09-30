7dba5d56c0d8b98dd4232aee676fe93954a05810
F. Sevde Pehlivan <fsevde.pehlivan@gmail.com>
2024-09-30 19:13:35 +0300

    Complete comments

---
+++ src/routes/comments.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const Comment = require("../models/comment");
+
+router.get("/", async (req, res) => {
+  try {
+    const comments = await Comment.getAll();
+    res.json(comments);
+  } catch {
+    res.status(400).json({ message: "Hata oldu. Tekrar deneyiniz." });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const comment = await Comment.getById(req.params.id);
+    if (!comment) {
+      res.status(404).json({ message: "Böyle bir kayıt bulunamadı." });
+    }
+    res.json(comment);
+  } catch {
+    res.status(400).json({ message: "Hata oldu. Tekrar deneyiniz." });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const comment = await Comment.create(req.body);
+    res.status(201).json(comment);
+  } catch {
+    res.status(400).json({ message: "Hata oldu. Tekrar deneyiniz." });
+  }
+});
+
+router.post("/:id", async (req, res) => {
+  try {
+    const updated = await Comment.update(req.params.id, req.body);
+    res.json(updated);
+  } catch {
+    res.status(400).json({ message: "Hata oldu. Tekrar deneyiniz." });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleted = await Comment.delete(req.params.id);
+    res.json(deleted);
+  } catch (err) {
+    console.log("err", err);
+    res.status(400).json({ message: "Hata oldu. Tekrar deneyiniz." });
+  }
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const categoryRoutes = require("./routes/category");
 const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
 
 const app = express();
 
 app.use(express.json());
 app.use("/categories", categoryRoutes);
 app.use("/posts", postRoutes);
+app.use("/comments", commentRoutes);
 
 const PORT = 3000;
 
